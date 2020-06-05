c9855642fa3df5555f6f58e2ea9fcb57dc1a4239
CrugBarat <craigtbarratt@gmail.com>
2020-06-05 20:56:26 +0100

    adds Drums subclass with constructor
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/instruments/Drums.java
@@ -0,0 +1,8 @@
+package instruments;
+
+public class Drums extends Instrument {
+
+    public Drums(String brand, String model, String type, double cost, double retail) {
+        super(brand, model, type, cost, retail);
+    }
+}
