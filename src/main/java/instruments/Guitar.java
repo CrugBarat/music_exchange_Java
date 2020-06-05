4320343717d858bdbc3959c6fc8cb6d48f15d52d
CrugBarat <craigtbarratt@gmail.com>
2020-06-05 19:19:17 +0100

    adds Guitar subclass with constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/instruments/Guitar.java
@@ -0,0 +1,59 @@
+package instruments;
+
+public class Guitar extends Instrument {
+
+    private String frets;
+    private int scale;
+    private String fretboard;
+    private String body;
+    private String neck;
+
+    public Guitar(String brand, String model, String type, double cost, double retail, String frets, int scale, String fretboard, String body, String neck) {
+        super(brand, model, type, cost, retail);
+        this.frets = frets;
+        this.scale = scale;
+        this.fretboard = fretboard;
+        this.body = body;
+        this.neck = neck;
+    }
+
+    public String getFrets() {
+        return frets;
+    }
+
+    public int getScale() {
+        return scale;
+    }
+
+    public String getFretboard() {
+        return fretboard;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getNeck() {
+        return neck;
+    }
+
+    public void setFrets(String frets) {
+        this.frets = frets;
+    }
+
+    public void setScale(int scale) {
+        this.scale = scale;
+    }
+
+    public void setFretboard(String fretboard) {
+        this.fretboard = fretboard;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setNeck(String neck) {
+        this.neck = neck;
+    }
+}
