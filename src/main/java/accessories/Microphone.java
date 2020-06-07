f9535c5d09cac1adcf5f9e410eacbea7bb44f733
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 18:35:34 +0100

    adds Microphone subclass with constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/accessories/Microphone.java
@@ -0,0 +1,29 @@
+package accessories;
+
+public class Microphone extends Accessory {
+
+    private double cableLength;
+    private String connectiontYPE;
+
+    public Microphone(String brand, String model, double cableLength, String connectiontYPE) {
+        super(brand, model);
+        this.cableLength = cableLength;
+        this.connectiontYPE = connectiontYPE;
+    }
+
+    public double getCableLength() {
+        return cableLength;
+    }
+
+    public String getConnectiontYPE() {
+        return connectiontYPE;
+    }
+
+    public void setCableLength(double cableLength) {
+        this.cableLength = cableLength;
+    }
+
+    public void setConnectiontYPE(String connectiontYPE) {
+        this.connectiontYPE = connectiontYPE;
+    }
+}
