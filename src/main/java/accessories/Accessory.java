f151ff2a0f2b2ea093b4cf61dc168a8fa1a6e791
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 20:51:42 +0100

    adds cost and retailPrice vars to Accessory parent with getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/accessories/Accessory.java
+++ src/main/java/accessories/Accessory.java
@@ -4,10 +4,14 @@ public abstract class Accessory {
 
     private String brand;
     private String model;
+    private double cost;
+    private double retail;
 
-    public Accessory(String brand, String model) {
+    public Accessory(String brand, String model, double cost, double retail) {
         this.brand = brand;
         this.model = model;
+        this.cost = cost;
+        this.retail = retail;
     }
 
     public String getBrand() {
@@ -18,11 +22,27 @@ public abstract class Accessory {
         return model;
     }
 
+    public double getCost() {
+        return cost;
+    }
+
+    public double getRetail() {
+        return retail;
+    }
+
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setRetail(double retail) {
+        this.retail = retail;
     }
 }
