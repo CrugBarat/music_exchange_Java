f31275de272d3613c82932ccf03ee530fb381d34
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 18:18:47 +0100

    adds Pedal subclass with constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/accessories/Pedal.java
@@ -0,0 +1,31 @@
+package accessories;
+
+import instruments.Instrument;
+
+public class Pedal extends Accessory{
+
+    private Instrument instrument;
+    private double cableLength;
+
+    public Pedal(String brand, String model, Instrument instrument, double cableLength) {
+        super(brand, model);
+        this.instrument = instrument;
+        this.cableLength = cableLength;
+    }
+
+    public Instrument getInstrument() {
+        return instrument;
+    }
+
+    public double getCableLength() {
+        return cableLength;
+    }
+
+    public void setInstrument(Instrument instrument) {
+        this.instrument = instrument;
+    }
+
+    public void setCableLength(double cableLength) {
+        this.cableLength = cableLength;
+    }
+}
