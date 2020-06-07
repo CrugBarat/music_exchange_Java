1e2586f66325fc026a3abbadb4d2e05efbdb1fbc
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 18:31:29 +0100

    adds Metronome subclass with constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/accessories/Metronome.java
@@ -0,0 +1,39 @@
+package accessories;
+
+public class Metronome extends Accessory{
+
+    private int tempoStartRange;
+    private int tempoEndRange;
+    private String powerSupply;
+
+    public Metronome(String brand, String model, int tempoStartRange, int tempoEndRange, String powerSupply) {
+        super(brand, model);
+        this.tempoStartRange = tempoStartRange;
+        this.tempoEndRange = tempoEndRange;
+        this.powerSupply = powerSupply;
+    }
+
+    public int getTempoStartRange() {
+        return tempoStartRange;
+    }
+
+    public int getTempoEndRange() {
+        return tempoEndRange;
+    }
+
+    public String getPowerSupply() {
+        return powerSupply;
+    }
+
+    public void setTempoStartRange(int tempoStartRange) {
+        this.tempoStartRange = tempoStartRange;
+    }
+
+    public void setTempoEndRange(int tempoEndRange) {
+        this.tempoEndRange = tempoEndRange;
+    }
+
+    public void setPowerSupply(String powerSupply) {
+        this.powerSupply = powerSupply;
+    }
+}
