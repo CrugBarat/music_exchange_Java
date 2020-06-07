bda9e632e8bde24d71ea43cc01e461af8653e568
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 18:43:01 +0100

    adds Flute subclass with constructor/getters/setters and implements IPlay
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/instruments/Flute.java
@@ -0,0 +1,35 @@
+package instruments;
+
+import behaviours.IPlay;
+
+public class Flute extends Instrument implements IPlay {
+
+    private String bodyMaterial;
+    private String keyOf;
+
+    public Flute(String brand, String model, String type, double cost, double retail, String bodyMaterial, String keyOf) {
+        super(brand, model, type, cost, retail);
+        this.bodyMaterial = bodyMaterial;
+        this.keyOf = keyOf;
+    }
+
+    public String getBodyMaterial() {
+        return bodyMaterial;
+    }
+
+    public String getKeyOf() {
+        return keyOf;
+    }
+
+    public void setBodyMaterial(String bodyMaterial) {
+        this.bodyMaterial = bodyMaterial;
+    }
+
+    public void setKeyOf(String keyOf) {
+        this.keyOf = keyOf;
+    }
+
+    public String play() {
+        return "Tu-re-lu-re-lu";
+    }
+}
