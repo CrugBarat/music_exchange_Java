cc551c1bc1cdb0bd20217d4ac11e0c2a55630256
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 18:39:12 +0100

    adds Cello subclass with constructor/getters/setters
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/main/java/instruments/Cello.java
@@ -0,0 +1,39 @@
+package instruments;
+
+public class Cello extends Instrument {
+
+    private String size;
+    private String body;
+    private String finish;
+
+    public Cello(String brand, String model, String type, double cost, double retail, String size, String body, String finish) {
+        super(brand, model, type, cost, retail);
+        this.size = size;
+        this.body = body;
+        this.finish = finish;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getFinish() {
+        return finish;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setFinish(String finish) {
+        this.finish = finish;
+    }
+}
