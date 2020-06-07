1d10d9647d4b5d8ef21ea741a84189bc5eb47a4a
CrugBarat <craigtbarratt@gmail.com>
2020-06-07 18:19:29 +0100

    extends Accessory parent to Case subclass
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/main/java/accessories/Case.java
+++ src/main/java/accessories/Case.java
@@ -2,11 +2,12 @@ package accessories;
 
 import instruments.Instrument;
 
-public class Case {
+public class Case extends Accessory{
 
     private Instrument instrument;
 
-    public Case(Instrument instrument) {
+    public Case(String brand, String model, Instrument instrument) {
+        super(brand, model);
         this.instrument = instrument;
     }
 
